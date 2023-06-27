66d65bf30a38f394f29cd945ab7df4af9f1f89aa
Chwila <bartosz.chwila@sap.com>
2023-06-27 12:51:44 +0200

    Configuration for release

--- scripts/create_draft_release.sh
+++ scripts/create_draft_release.sh
@@ -15,7 +15,7 @@ GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
 GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
 CHANGELOG_FILE=$(cat CHANGELOG.md)
 RELEASE_NOTES_FILE=$(cat "docs/release-notes/${RELEASE_TAG}.md")
-BODY="${CHANGELOG_FILE}\n${RELEASE_NOTES_FILE}"
+BODY="${CHANGELOG_FILE}"
 
 JSON_PAYLOAD=$(jq -n \
   --arg tag_name "$RELEASE_TAG" \
