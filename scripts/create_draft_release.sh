442004a9349fa961f795c076485840d64aa867fd
Chwila <bartosz.chwila@sap.com>
2023-06-27 12:34:55 +0200

    Configuration for release

--- scripts/create_draft_release.sh
+++ scripts/create_draft_release.sh
@@ -14,8 +14,8 @@ REPOSITORY=${REPOSITORY:-kyma-project/keda-manager}
 GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
 GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
 CHANGELOG_FILE=$(cat CHANGELOG.md)
-RELEASE_NOTES_FILE=$(cat "docs/release-notes/${RELEASE_TAG}.md")
-BODY="${CHANGELOG_FILE}\n${RELEASE_NOTES_FILE}"
+#RELEASE_NOTES_FILE=$(cat "docs/release-notes/${RELEASE_TAG}.md")
+BODY="${CHANGELOG_FILE}"
 
 JSON_PAYLOAD=$(jq -n \
   --arg tag_name "$RELEASE_TAG" \
