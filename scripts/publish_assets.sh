445332b6d016075a5db717839f93483ef42884f8
Chwila <bartosz.chwila@sap.com>
2023-06-27 15:58:27 +0200

    url

--- scripts/publish_assets.sh
+++ scripts/publish_assets.sh
@@ -15,11 +15,11 @@ REPOSITORY=${REPOSITORY:-barchw/istio}
 GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
 GITHUB_AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
 
-curl -L \
+curl -f -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "${GITHUB_AUTH_HEADER}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   -H "Content-Type: text/yaml" \
-  ${GITHUB_URL}/releases/${RELEASE_ID}/assets?name=manifests.yaml \
-  --data-binary manifests.yaml
+  --data-binary manifests.yaml \
+  "${GITHUB_URL}/releases/${RELEASE_ID}/assets?name=manifests.yaml"
