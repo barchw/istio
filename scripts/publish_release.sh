1bb2904d051674c18ed4110f695a83b582cdf1ea
Chwila <bartosz.chwila@sap.com>
2023-07-04 12:29:51 +0200

    Add pre-release boolean

--- scripts/publish_release.sh
+++ scripts/publish_release.sh
@@ -9,6 +9,7 @@ set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
 RELEASE_ID=$1
+IS_PRE_RELEASE=$2
 
 REPOSITORY=${REPOSITORY:-barchw/istio}
 GITHUB_URL=https://api.github.com/repos/${REPOSITORY}
@@ -20,4 +21,4 @@ curl -f -L \
   -H "${GITHUB_AUTH_HEADER}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   ${GITHUB_URL}/releases/${RELEASE_ID} \
-  -d '{"draft":false}'
+  -d "{\"draft\":${IS_PRE_RELEASE}"
